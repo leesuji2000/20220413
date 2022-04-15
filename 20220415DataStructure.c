185fcb897cb8d5acbb79861888204fc8fc21d6f8
sujee2020 <64798587+leesuji2000@users.noreply.github.com>
2022-04-15 16:54:49 +0900

    Add files via upload

---
+++ 20220415DataStructure.c
@@ -0,0 +1,170 @@
+//자료구조의 필요성: 데이터를 효과적으로 저장하고 처리하는 방법(불필요한 낭비 줄임)
+//자료구조의 종류
+//선형구조: 배열, 연결리스트, 스택, 큐
+//비선형구조: 트리, 그래프
+
+
+//프로그램 성능 측정 방법론
+//시간복잡도(time complexity): 알고리즘에 사용되는 연산횟수
+//공간복잡도(space complexity): 알고리즘에 사용되는 메모리의 양
+
+
+//시간복잡도를 표현할 때, 최악의 경우를 나타내는 Big-O 표기법을 사용
+//O(n) -> 하나의 반복문 , O(n^2) -> 이중 반복문
+//n = 1000번 연산, nlonn = 10000번 연산, n^2 = 1000000의 연산, ..
+//보통 10억연산이 넘어가면 1초 이상이 걸린다.
+
+//공간복잡도는 MB단위
+//int a[1000]: 4KB, int a[1000000]: 4MB, int a[2000][2000]: 16MB
+
+//연결리스트 필요성, 쓰임새 
+//연결리스크 필요성: 배열을 사용하여 데이터를 순차적으로 저장하고 나열할 수 있는데
+//배열을 사용하는 경우 메모리공간이 불필요하게 낭비될 수 있다.
+
+//배열 기반의 리스트
+//데이터를 순차적으로 저장하고, 처리할 떄는 배열 기반의 리스트를 간단히 이용할 수 있다
+//#include <stdio.h>
+//#define INF 1000
+//
+//int arr[INF];
+//int count = 0;
+//
+//void addBack(int data) {
+//	arr[count] = data;
+//	count++;
+//}
+//void addFirst(int data) {
+//	for (int i = count; i > 0; i--) {
+//		arr[i] = arr[i - 1];
+//
+//	}
+//	arr[0] = data;
+//	count++;
+//}
+//void removeAt(int index) {
+//	for (int i = index; i < count - 1; i++) {
+//		arr[i] = arr[i + 1];
+//	}
+//	count--;
+//}
+//void show() {
+//	for (int i = 0; i < count; i++) {
+//		printf("%d ", arr[i]);
+//	}
+//}
+//int main(void) {
+//	addFirst(4);
+//	addFirst(5);
+//	addBack(7);
+//	addBack(7);
+//	addFirst(9);
+//	addFirst(10);
+//	removeAt(5);
+//	show();
+//	system("pause");
+//	return 0;
+//}
+
+//배열 기반 리스트 특징
+//배열로 만들었으므로 특정한 위치의 원소에 즉시 접근할 수 있다.
+//데이터가 들어갈 공간을 미리 메모리에 할당해야 한다는 단점
+//원하는 위치로의 삽입이나 삭제가 비효율적
+
+//일반적으로 연결리스트는 구조체와 포인터를 함께 사용하여 구현
+//연결리스트는 리스트 중간지점에 노드를 추가하거나 삭제할 수 있어야 한다.
+//필요한 때마다 메모리 공간을 할당 받는다.(동적할당 이용)
+
+//단일 연결 리스트 (data | next) -> (data | next) -> (data | next)
+//하나의 구조체에 data와 다음 노드를 가리키는 next가 존재
+//포인터를 이용해서 단방향적으로 다음 노드를 가리킴
+//일반적으로 연결리스트의 시작 노드를 head라고 부르고 별도로 관리
+//마지막 노드의 next가 가리키는 값은 없으므로 null값을 넣는다.
+
+//연결리스트 구조체 만들기
+#define _CRT_SECURE_NO_WARNINGS_
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int data;
+	struct Node* next; //그 다음 변수를 가리키는 next 구조체 변수 선언
+} Node;
+Node* head;//항상 연결리스트는 head로 시작하기 때문에 head 노드를 만들어줘야한다.(동적할당을 할것이기 때문에 포인터 사용)
+////연결리스트 구조체 사용하기
+
+
+//int main(void) {
+//	head = (Node*)malloc(sizeof(Node));
+//	Node* node1 = (Node*)malloc(sizeof(Node));
+//	node1->data = 1;
+//	Node* node2 = (Node*)malloc(sizeof(Node));
+//	node2->data = 2;
+//	head->next = node1;
+//	node1->next = node2;
+//	node2->next = NULL;
+//	Node* cur = head->next;
+//	while (cur != NULL) {
+//		printf("%d ", cur->data);
+//		cur = cur->next;
+//	}
+//	system("pause");
+//	return 0;
+//}
+//연결리스트 삽입과정
+void add(Node*root, int data) {
+	Node *node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	node->next = root->next;
+	root->next = node;
+}
+
+
+//연결리스트 삭제과정
+void del(Node* root) {
+	Node* front = root->next;
+	root->next = front->next;
+	free(front);
+}
+
+//연결 리스트 메모리 해제 함수
+void freeAll(Node* root) {
+	Node* cur = head->next;
+	while (cur != NULL) {
+		Node* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+}
+//연결리스트 전체 출력 함수
+ 
+void showAll(Node* root) {
+	Node* cur = head->next;
+	while (cur != NULL) {
+		printf("%d ", cur->data);
+		cur = cur->next;
+	}
+}
+
+int main(void) {
+	head = (Node*)malloc(sizeof(Node));
+	head->next = NULL;
+	add(head, 1);
+	add(head, 0);
+	add(head, -1);
+	del(head);
+	showAll(head);
+	freeAll(head);
+	system("pause");
+	return 0;
+}
+//연결리스트 구현 주의점
+//소스코드에 덧붙여서 삽입, 삭제 기능에서의 예외상항을 처리할 필요가 있다.
+//삭제할 원소가 없는데 삭제하는 경우, head노드 자체를 잘못 넣는 경우 등을 체크해야한다
+
+//연결리스트의 특징
+//삽입과 삭제가 배열에 비해 간단
+//배열과 다르게 특정 인덱스에 접근하지 못하고, 원소를 차례대로 검색
+//추가적인 포인터 변수가 사용되어 메모리 공간 낭비
+//연결리스트는 데이터를 선형적으로 저장하고 처리하는 방법
+//기존에 배열을 이용했을때보다 삽입과 삭제가 많은 경우에서 효율적
+//특정 인덱스에 바로 참조해야할때가 많다면 배열을 이용하는 것이 효율적
--- review.c
+++ review.c
@@ -1,22 +1,26 @@
-#include <stdio.h>
-int main(void) {
-    char* a = "hello world"; //포인터 변수에 문자열을 상수처럼 넣는것 = 문자열 리터럴(변경불가능)
-    //변경하고 싶으면 포인터 변수 a가 가르키는 문자열을 다른 것으로 바꾸면 된다.
-    char** b = a;
-    printf("%s\n", a); //문자열은 내부적으로 배열이고, 포인터와 치환될 수 있기 때문에 *a를 안쓴것이다.
-    printf("%d\n", a);
-    printf("%s\n", b);
-
-    printf("%d\n", *b);
-    printf("%d\n", &a);
-
-    printf("%d\n", *a); //hello world에서 첫글자 h가 나타내는 아스키코드값
-    printf("%d\n", 'h');
-    printf("%d\n", **b);
-    
-    system("pause");
-    return 0;
-}
+//#include <stdio.h>
+//int main(void) {
+//    char* a = "hello world"; //포인터 변수에 문자열을 상수처럼 넣는것 = 문자열 리터럴(변경불가능)
+//    //변경하고 싶으면 포인터 변수 a가 가르키는 문자열을 다른 것으로 바꾸면 된다.
+//    char** b = a;
+//    //printf("%s\n", a); //문자열은 내부적으로 배열이고, 포인터와 치환될 수 있기 때문에 *a를 안쓴것이다.
+//    //printf("%d\n", a);
+//    //printf("%s\n", b);
+//    //printf("%d\n", b);
+//
+//
+//    printf("%d\n", &b);
+//    printf("%d\n", &a);
+//
+//    //printf("%d\n", *a); //hello world에서 첫글자 h가 나타내는 아스키코드값
+//    //printf("%d\n", 'h');
+//    //printf("%d\n", **b);
+//    
+//    system("pause");
+//    return 0;
+//}
+////a 는 문자열을 나타내고, &a는 a의 주소값을 나타나내고, *a는 문자열의 첫번째 문자를 나타낸다??
+////그러면 b는 문자열을 나타내고, *b는 a의 주소값을 나타내야하는거 아닌가?
 
 
 //동적으로 문자열 처리하기
@@ -24,63 +28,39 @@ int main(void) {
 //memset(포인터, 값, 크기);
 //한 바이트 씩 값을 저장하므로 문자열 배열의 처리방식과 비슷 -> <string.h>에 정의됨
 
-//memset()함수를 이용해서 A로 채워진 메모리할당
 //#include <stdio.h>
 //#include <stdlib.h>
-//int main(void) {
-//	char* a = malloc(100);
-//	memset(a, 'A', 100);
-//	for (int i = 0; i < 100; i++) {
-//		printf("%c ", a[i]);
-//	}
-//	system("pause");
-//	return 0;
-//}
-
-//동적메모리할당 + 2중 포인터
-//3*3 표 만든 후에 값 출력하기
-//#include <stdio.h>
-//#include <stdlib.h>
-//int main(void) {
-//	int** p = (int**)malloc(sizeof(int*) * 3);
-//	for (int i = 0; i < 3; i++) {
-//		*(p + i) = (int*)malloc(sizeof(int) * 3);
-//	}
-//	for (int i = 0; i < 3; i++) {
-//		for (int j = 0; j < 3; j++) {
-//			*(*(p + i) + j) = i * 3 + j;
-//		}
-//	}
-//	for (int i = 0; i < 3; i++) {
-//		for (int j = 0; j < 3; j++) {
-//			printf("%d ", *(*(p + i) + j));
+//#include <string.h>
 //
-//		}
-//		printf("\n");
-//	}
-//	system("pause");
-//	return 0;
+//int main() {
+//    int* a = malloc(100);
+//    memset(a, 3, 100* sizeof(int));
+//    for (int i = 0; i < 100; i++) {
+//        printf("%d ", a[i]);
+//    }
+//  
+//
+//    system("pause");
+//    return 0;
 //}
+//??왜 3이 출력 안되면 주소값이 출력 되는거야 *(a+i)해도 주소값 출력된다고
+//memset()함수는 1바이트씩만 처리하기 때문에 문자와 0이 아닌 이상 처리할 수 없어서 주소값이 나온다.
+//memset()함수를 이용할 때에는 문자 or 0으로만 초기화 해야한다
+
+
+
 
 //함수포인터
 //함수의 이름을 이용해서 특정한 함수를 호출, 함수이름은 메모리 주소를 반환
 
 //함수의 이름이 내부적으로 메모리 주소값을 갖는 실습
-//#include <stdio.h>
 
-//void function() {
-//	printf("It's my funciton");
-//}
-//int main(void) {
-//	printf("%d\n", function);
-//	system("pause");
-//	return 0;
-//}
 
 //함수 포인터: 특정한 함수의 반환 자료형을 지정하는 방식으로 선언
 //함수 포인터를 이용하면 형태가 같은 서로 다른 기능의 함수를 선택적으로 사용할 수 있다
 //반환 자료형 (*함수명) (매개변수) = 함수명;
 //특정한 함수를 가리키는 포인터를 만들 수 있는것
+
 
 //매개변수 및 반환 자료형이 없는 함수 포인터
 //#include <stdio.h>
@@ -131,48 +111,8 @@ int main(void) {
 //	system("pause");
 //	return 0;
 //}
-//함수포인터를 자주 사용하지 않기 때문에 컴퓨터의 구조를 이용하는데 도우미 된다
+//함수포인터를 자주 사용하지 않기 때문에 컴퓨터의 구조를 이용하는데 도움이 된다
 //c언어 프로그램의 모든 함수는 내부적으로 포인터 형태로 관리할 수 있다.
-
-//구조체: 여러개의 변수를 묶어서 하나의 객체를 표현할 수 있다.
-//배열과는 다르게 다른 자료형의 변수를 묶어서, 다양한 객체를 표현할 수 있다.
-// struct 구조체명 { 자료형1 변수명; ...}
-//구조체의 변수 선언와 접근(.을 이용해서 구조체변수에 접근한다)
-//struct 구조체변수명 구조체변수; //구조체 변수 선언
-//string 함수를 이용해서 복사(strcpy)한다.
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <string.h>
-//struct student {
-//	char name[10];
-//	int age;
-//	char grade[10];
-//};
-//int main(void) {
-//	struct student s; //struct student s = {"leesuji", 23, "A"} 차례대로 넣어 초기화할 수 있음
-//	strcpy(s.name, "leesuji");
-//	s.age = 23;
-//	strcpy(s.grade, "A");
-//	printf("%s\n %d\n%s", s.name, s.age);
-//	printf("%s\n", s.grade);
-//	
-//	system("pause");
-//	return 0;
-//}
-//
-//
-//struct student {
-//	char name[10];
-//	int age;
-//	char grade[10];
-//} s; //이렇게 쓰면 구조체 선언과 동시에 전역변수(구조체변수) s도 선언할 수 있다
-//
-//
-//typedef struct student {
-//	char name[10];
-//	int age;
-//	char grade[10];
-//} student; //typedef를 사용하면 나중에 구조체 선언할때 student s;라고 선언한면 된다. struct를 안붙여도됨
 
 
 //구조체가 포인터 변수로 사용되는 경우) 내부 변수에 접근할 때 화살표(->)를 사용한다
@@ -280,4 +220,89 @@ int main(void) {
 //	printf("%.2f\n", (double)sum / n);
 //	system("pause");
 //	return 0;
+//}
+
+//전처리기: 다른 프로그램 영역과 독립적으로 처리된다, 소스코드 파일 단위로 효력 존재
+
+//파일 포함 전처리기
+//#include 는 전처리기에서 가장 많이 사용되는 문법, 라이브러리로 포함시킨다
+//#include를 이용하면 어떤 파일도 가져올 수 있다.
+//#include <파일이름> : 시스템 디렉토리에서 파일을 검색, 운영체제마다 시스템 디렉토리가 존재하는 경로가 다를 수 있다
+//#include "파일이름" : 현재 폴더에서 파일을 먼저 검색후, 없다면 시스템 디렉토리에서 파일을 검색
+
+//#include <stdio.h>
+//#include "temp.h"
+//
+//int main(void) {
+//	printf("%d\n", add(2, 3));
+//	system("pause");
+//	return 0;
+//}
+
+//매크로 전처리기: 프로그램 내에서 사용되는 상수나 함수를 매크로 형태로 저장하기 위해 사용
+//일반적으로 자주 사용한는 상수나 함수를 매크로 전처리기(#define)를 이용해서 나타낸다.
+
+//#include <stdio.h>
+//#define PI 3.14
+//int main(void) {
+//	int r = 3;
+//	printf("%.2f\n", 2*PI*r);
+//	system("pause");
+//	return 0;
+//}
+
+//인자를 가지는 매크로 전처리기: 함수와 같은 형태로 인자를 받을 수 있다.
+//#include <stdio.h>
+//#define POW(x) (x*x)
+//int main(void) {
+//	int r = 3;
+//	printf("%d\n", POW(r));
+//	system("pause");
+//	return 0;
+//}
+
+//전처리기 #define 은 코드를 쓰는 시간을 획기적으로 줄여준다.
+//#include <stdio.h>
+//#define ll long long
+//#define ld long double
+//int main(void) {
+//	ll a = 2324642677;
+//	ld b = 31515.3466;
+//	printf("%.2f\n", a * b);
+//	system("pause");
+//	return 0;
+//}
+
+//조건부 컴파일: 컴파일이 이루어지는 영역을 지정하는 기법
+//일반적으로 디버깅과 소스코드 이식을 목적으로 작성
+//c언어로 시스템 프로그램을 작성할때, 운영체제에 따라서 소스코드가 달라질 수 있다.
+//이떄 운영체제에 따라서 컴파일이 수행되는 소스코드를 다르게 할 수 있다.
+//하드웨어에 가까운 운영체제를 다룰때, 각각에 맞는 소스파일을 넣어야 할 때 사용한다.
+
+//조건부 컴파일 #ifndef ~ #endif 문법
+//헤더 파일의 내용이 중복되어 사용되지 않도록 주의
+
+
+//#include <stdio.h>
+//#include "temp.h"
+//#include "temp.h"//temp.h파일에 있는 조건부 컴파일 #ifndef ~ #endif에 의해서 중복해서 작성해도 오류 안뜬다.
+//
+//int main(void) {
+//	printf("%d\n", add(2, 3));
+//	system("pause");
+//	return 0;
+//}
+
+//파일 분할 컴파일
+//일반적으로 자신이 직접 라이브러리를 만들때, c언어 파일과, 헤더파일을 모두 작성해야 한다.
+//분할을 통해서 좀 더 효율적이고 체계적인 관리를 할 수 있다
+
+//#include <stdio.h>
+//#include "temp1.h"
+//
+//
+//int main(void) {
+//    printf("%d\n", add(2, 3));
+//    system("pause");
+//    return 0;
 //}
