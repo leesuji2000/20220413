9c90b203547a550c7a153d8d7163b2ae7658e534
sujee2020 <64798587+leesuji2000@users.noreply.github.com>
2022-04-29 15:23:50 +0900

    Add files via upload

--- 20220418DataStructure.c
+++ 20220418DataStructure.c
@@ -53,72 +53,144 @@
 //불필요한 메모리공간을 낭비할 수 있다.
 //그래서 연결리스트(포인터)를 이용한 스택 구현
 
-#include<stdio.h>
-#include <stdlib.h>
-#define INF 9999999
-
-typedef struct {
-	int data;
-	struct Node* next;
-
-}Node;
-
-//연결리스트를 이용한 구현이기 때문에 위와 같이 연결리스트의 노드를 만들어준다.
-
-typedef struct {
-	Node* top;
-}Stack;
-//모든 스택은 top이라는 노드를 갖고 있어야 한다. 포인터를 갖고 있기 때문에 일종의 배열형태라고 할 수 있다.
-
-// (data | next )_top -> ( data | next ) -> (data | next ) -> NULL
-//삽입을 할때 top위치에 와야 한다.
-
-void push(Stack *stack, int data) {
-	Node* node = (Node*)malloc(sizeof(Node));
-	node->data = data;
-	node->next = stack->top;
-	stack->top = node;
-}
-
-void pop(Stack *stack) {
-	if (stack->top == NULL) {
-		printf("스택 언더플로우가 발생했습니다.");
-		return -INF;
-	}
-	Node* node = stack->top;
-	
-	stack->top = node->next;
-	free(node);
-	return node->data;
-}
-
-void show(Stack*stack) {
-	Node* cur = stack->top;
-	printf("---스택 최상단---\n");
-	while (cur != NULL) {
-		printf("%d\n", cur->data);
-		cur = cur->next;
-	}
-	printf("---스택 최하단");
-}
-
-int main(void) {
-	Stack s;
-	s.top = NULL;//꼭 넣어야한다. 언더플로우나 loop문을 막을 수 있다.
-
-	push(&s, 7);
-	push(&s, 5);
-	push(&s, 4);
-	pop(&s);
-	push(&s, 6);
-	pop(&s );
-	show(&s);
-	system("pause");
-	return 0;
-}
+//#include<stdio.h>
+//#include <stdlib.h>
+//#define INF 9999999
+//
+//typedef struct {
+//	int data;
+//	struct Node* next;
+//
+//}Node;
+//
+////연결리스트를 이용한 구현이기 때문에 위와 같이 연결리스트의 노드를 만들어준다.
+//
+//typedef struct {
+//	Node* top;
+//}Stack;
+////모든 스택은 top이라는 노드를 갖고 있어야 한다. 포인터를 갖고 있기 때문에 일종의 배열형태라고 할 수 있다.
+//
+//// (data | next )_top -> ( data | next ) -> (data | next ) -> NULL
+////삽입을 할때 top위치에 와야 한다.
+//삽입할 next가 현재 top이 되도록 만들고, 삽입할 node를 top이 되도록 만든다
+//
+//void push(Stack *stack, int data) {
+//	Node* node = (Node*)malloc(sizeof(Node));
+//	node->data = data;
+//	node->next = stack->top;
+//	stack->top = node;
+//}
+//현재 top이 가리키는 그 다음의 노드가 top이 되도록 한다 그 후에 할당 해제
+//void pop(Stack *stack) {
+//	if (stack->top == NULL) {
+//		printf("스택 언더플로우가 발생했습니다.");
+//		return -INF;
+//	}
+//	Node* node = stack->top;
+//	int data = node->data;
+//	stack->top = node->next;
+//	free(node);
+//	return node->data;
+//}
+//
+//void show(Stack*stack) {
+//	Node* cur = stack->top;
+//	printf("---스택 최상단---\n");
+//	while (cur != NULL) {
+//		printf("%d\n", cur->data);
+//		cur = cur->next;
+//	}
+//	printf("---스택 최하단");
+//}
+//
+//int main(void) {
+//	Stack s;
+//	s.top = NULL;//꼭 넣어야한다. 언더플로우나 loop문을 막을 수 있다.
+//
+//	push(&s, 7);
+//	push(&s, 5);
+//	push(&s, 4);
+//	pop(&s);
+//	push(&s, 6);
+//	pop(&s );
+//	show(&s);
+//	system("pause");
+//	return 0;
+//}
 
 //연결리스트를 이용해서 메모리의 낭비 없이 스택을 구현할 수 있다.
 //많이 활용되니 꼭 기억하길
 
 
+//스택을 활용한 계산기 만들기
+//중위 표기법을 후위 표기법으로 변환하는 방법을 이해할 수 있다.
+//후위 표기법을 계산하여 결과 값을 도출하는 방법을 이해할 수 있다.
+//결과적으로 스택 자료구조를 활용하여 '계산기'를 구현할 수 있다.
+
+//중위표기법
+//일반적으로 사람이 수식을 표기할 때, 사용하는 표기 방법
+// 예를 들어 7 * 5 + 3
+
+//후위표기법
+//컴퓨터가 연산하기 편한 수식
+//연산자는 뒤쪽에 위치함
+// 예를 들어 7 5 * 3 +
+
+//스택을 활용해 수식을 계산하는 방법
+// 1. 수식을 후위표기법으로 변환한다. -> 2. 후위 표기법을 계산하여 결과를 도출한다
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+	char data[100]; //숫자는 길 수 있기때문에 문자열로 받음
+	struct Node* next;
+	
+}Node;
+
+typedef struct Stack {
+	Node* top;
+}Stack;
+
+void push(Stack* stack, char *data) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	strcpy(node->data, data);
+	node->next = stack->top;
+	stack->top = node;
+}
+
+char* getTop(Stack* stack) {
+	Node* top = stack->top;
+	return top->data;
+}
+
+char* pop(Stack* stack) {
+	if (stack->top == NULL) {
+		printf("스택 언더플로 발생\n");
+		return NULL;
+
+	}
+	Node* node = stack->top;
+	char* data = (char*)malloc(sizeof(char) * 100);
+	strcpy(data, node->data);
+	stack->top = node->next;
+	free(node);
+	return data;
+}
+
+//중위표기법을 후위표기법으로 바꾸는 방법
+//1. 피연산작 들어오면 바로 출력
+//2. 연산자가 들어오면 자기보다 우선순위가 높거나 같은 것들을 빼고 자신을 스택에 담기
+//3. 여는 괄호 (를 만남녀 무조건 스택에 담기
+//4. 닫는 괄호 )를 만날때까지 스택에서 출력
+
+//후위 표기법으로 변환 - 우선순위 함수 만들기
+int getPriority(char* i) {
+	if (!strcmp(i, "(")) return 0;
+	if (!strcmp(i, "+")) || !strcmp(i, "-") return 1;
+	if (!strcmp(i, "*")) || !strcmp(i, "/") return 2;
+	return 3;
+}
 
